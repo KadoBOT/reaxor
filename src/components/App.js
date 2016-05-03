4cd0931af36f89dbba31eacd07e5202f242d4f90
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2016-05-02 22:55:26 -0300

    fixed some bugs

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,29 @@
 // @flow
 
-import React from 'react';
-import { observer } from 'mobx-react';
-import { css } from 'aphrodite';
-import {styles} from '../styles/styles.css';
-import MyComponent from './MyComponent';
+import React from 'react'
+import { observer } from 'mobx-react'
+import { css } from 'aphrodite'
+import {styles} from '../styles/styles.css'
+import MyComponent from './MyComponent'
 
 @observer
 class App extends React.Component {
-  render () {
+  render() {
 
-    const { store } = this.props;
+    const { store } = this.props.store
 
     return (
       <div>
-        <h2 className={css(styles.red)}>Name: {store.name}</h2>
-        <h3 className={css(styles.animate)}>Description: {store.description}</h3>
+        <h2 className={css(styles.red)}>Welcome to the {store.name} project!</h2>
+        <h3 className={css(styles.animate)}>This project is {store.description}.</h3>
         <MyComponent store={store} />
       </div>
-    );
+    )
   }
 }
 
 App.propTypes = {
-  store: React.PropTypes.object
-};
+  store: React.PropTypes.object,
+}
 
-export default App;
+export default App
